623c3f8e1d5280c630335dcc7d0c0767a4e96cf4
Chris LaRocque <larocque.christopher@gmail.com>
2023-01-24 12:25:21 -0500

    Mobile nav no longer janky

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -12,26 +12,22 @@ type NavProps = {
 export default function Nav(props: NavProps) {
   const { title, items } = props;
   return (
-    <nav
-      style={{ border: "1px solid red" }}
-      className="items flex w-full items-center justify-between p-2 text-right lg:text-left"
-    >
+    <nav className="flex items-center justify-between p-2 text-right lg:text-left">
       <strong className="text-xl font-bold">{title}</strong>
-      <div className="nav-links relative ">
+      <div className="nav-links">
         <input
-          className="checkbox peer w-9 opacity-0 lg:hidden"
+          className="checkbox peer hidden w-9 opacity-0"
           type="checkbox"
           id="hamburger-toggle"
           aria-label="Toggle Navigation"
         />
         <label
-          style={{ border: "1px solid red" }}
-          className="absolute top-0  right-0 flex h-12 w-12 flex-col justify-around p-2 lg:hidden"
+          className="flex h-7 w-7 flex-col justify-around p-1 lg:hidden"
           htmlFor="hamburger-toggle"
         >
-          <span className="block h-2 w-full bg-black"></span>
-          <span className="block h-2 w-full bg-black"></span>
-          <span className="block h-2 w-full bg-black"></span>
+          <span className="block h-1 w-full bg-black"></span>
+          <span className="block h-1 w-full bg-black"></span>
+          <span className="block h-1 w-full bg-black"></span>
         </label>
 
         <ul className="absolute right-0 top-16 hidden w-full peer-checked:block lg:relative lg:inset-0 lg:flex">
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -54,54 +54,8 @@ export default function IndexPage() {
     { to: "/", text: "Link 2" },
   ];
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <title>Gatsby Starter Tailwind Components</title>
+    <>
       <Nav title="Tailwind" items={navItems} />
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="py-16 text-6xl font-bold">
-          Welcome to{" "}
-          <a className="text-purple-600" href="https://www.gatsbyjs.com/">
-            Gatsby.js!&nbsp;
-          </a>
-          <span role="img" aria-label="Party popper emojis">
-            🎉🎉🎉
-          </span>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{" "}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.jsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          {links.map((link) => (
-            <a
-              href={link.url}
-              className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-purple-600 focus:text-purple-600"
-            >
-              <h3 className="text-2xl font-bold">{link.text} &rarr;</h3>
-              <p className="mt-4 text-xl">{link.description}</p>
-            </a>
-          ))}
-        </div>
-      </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://gatsbyjs.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <img
-            alt="Gatsby G Logo"
-            src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-          />
-        </a>
-      </footer>
-    </div>
+    </>
   );
 }

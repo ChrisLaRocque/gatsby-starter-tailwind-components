973673bffee88eb801459a353f5c7bf4cc95eab1
Chris LaRocque <larocque.christopher@gmail.com>
2023-03-22 18:58:06 -0400

    Update Tabs.tsx

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -20,11 +20,11 @@ export default function Tabs() {
     <section>
       <div className="tab-wrapper mx-auto max-w-7xl py-7">
         <Tab.Group>
-          <Tab.List className="mt-6 flex w-full justify-between border-t border-slate-200 sm:gap-0 lg:gap-x-5 ">
+          <Tab.List className="mt-6 block w-full justify-between border-t border-slate-200 sm:gap-0 lg:flex lg:gap-x-5 ">
             {data.map(({ label }) => {
               return (
                 <Tab
-                  className="w-full border-t border-transparent py-7  px-3 text-left ui-selected:border-indigo-500 [&:not(:focus-visible)]:focus:outline-none"
+                  className="w-full border-t border-transparent py-7 px-3 text-left ui-selected:border-indigo-500 [&:not(:focus-visible)]:focus:outline-none"
                   key={label.heading}
                 >
                   <h4 className="font-bold">{label.heading}</h4>
